3452505e6ce0db912001fd92e92510e6b69c0430
Yuşa Abdullah AYAR <yusaabdullahayar@gmail.com>
2022-10-05 14:54:56 +0300

    Ürün Satış Takip
    C# - SQL  ile Ürün takip sistemi

---
+++ Hareket Takip/Hareket Takip/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace Hareket_Takip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-RBCL1FM\SQLEXPRESS;Initial Catalog=HareketTakip;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute HareketTakip",baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            
+        }
+    }
+}
